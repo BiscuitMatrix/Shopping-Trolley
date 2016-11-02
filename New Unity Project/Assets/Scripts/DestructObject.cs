94a052c3f6e2ae810030dd4c6cb0b1af21cf0d25
GeorgeGerald <slowpokegerald95@gmail.com>
2016-11-02 19:59:49 +0000

    updated files

---
+++ New Unity Project/Assets/Scripts/DestructObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructObject : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(other.name);
+        Destroy(gameObject); // When triggered object destroys itself
+    }
+}
